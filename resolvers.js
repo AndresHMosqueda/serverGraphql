ef3fab70f168cb00a277f94428ad5653bd8ddddc
AndresHMosqueda <andreshm94@gmail.com>
2020-09-06 13:24:14 -0400

    Done

--- resolvers.js
+++ resolvers.js
@@ -5,6 +5,17 @@ const Query = {
     job: (root, {id}) => db.jobs.get(id),
     jobs: () => db.jobs.list()
 }
+const Mutation = {
+    createJob: (root, {input}, {user}) => {
+        //check user auth
+       if(!user){
+           throw new Error('Unauthorized');
+       }
+        const id =  db.jobs.create({...input, companyId: user.companyId});
+        return db.jobs.get(id);
+    }
+};
+
 const Company = {
     jobs: (company) => db.jobs.list().filter( job => job.companyId === company.id)
 }
@@ -13,4 +24,4 @@ const Job = {
 }
 
 
-module.exports = {Query, Job, Company}
+module.exports = {Query, Job, Mutation, Company}
